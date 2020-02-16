968e1d1ce2675148d43cd29c065b43457b4431a0
Paul McManus <pmcmanus2021@gmail.com>
2020-02-16 12:32:59 +0000

    After Multiple Components Lesson

---
+++ src/containers/CommentBox.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import CommentList from "../components/CommentList";
+
+class CommentBox extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+          data: [
+            { id: 1,
+              author: "Lindsey Izzard",
+              text: "I love JS more each day."
+            },
+            { id: 2,
+              author: "Ally McGilloway",
+              text: "Just wait until we add the form. It's gonna be so good."
+            }
+          ]
+        };
+      }
+
+
+  render() {
+    return (
+      <div className="comment-box">
+        <h2>Comments</h2>
+        <CommentList data={this.state.data} />
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
